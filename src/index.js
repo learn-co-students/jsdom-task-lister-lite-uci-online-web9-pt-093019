17b7a7b74dc50d6d0e353603ffbb2557dd857088
Alice Freda <alicefreda@gmail.com>
2020-04-30 19:13:42 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+let counter = 0;
+const taskForm = document.getElementById("create-task-form");
+
+  function addTask() { 
+    let taskList = document.getElementById("tasks");
+    let newTaskDesc = document.getElementById("new-task-description");
+    let newTaskPriority = document.getElementById("new-task-priority");
+    const lineItem = document.createElement("li");
+
+    lineItem.textContent = `${newTaskDesc.value} in category: ${newTaskCat.value}`;
+    lineItem.id = `item-${counter}`
+    taskList.appendChild(lineItem);
+
+    //Add delete button
+    let deleteButton = document.createElement("button");
+    deleteButton.innerText = "x";
+    lineItem.append(deleteButton);
+    //Clicking button deletes current lineItem
+      deleteButton.addEventListener("click", function(e) {
+        // select what to delete
+        const buttonParent = deleteButton.parentElement
+        // remove it
+        buttonParent.remove();  
+      });
+    newTaskDesc.value = "";
+    counter += 1;
+  };
+
+  taskForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+    addTask();
+  });
+
+  
 });
